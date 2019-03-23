06af7064030f1179d55e247f7fc1c5ded7b931f9
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2019-03-23 15:50:39 +0100

    Implement HeartRateSensor

--- c/heartratesensor/src/HeartRateSensor.c
+++ c/heartratesensor/src/HeartRateSensor.c
@@ -1,14 +1,26 @@
 
 #include <fogPlatform.h>
-
 #include <HeartRateSensor.h>
+#include <Random.h>
+
+#define MIN_HEART_RATE 40
+#define MAX_HEART_RATE 200
 
 struct HeartRateSensor {
-	
+	Random random;
 };
 
 HeartRateSensor newHeartRateSensor() {
-	return calloc(1, sizeof(struct HeartRateSensor));
+	HeartRateSensor sensor = calloc(1, sizeof(struct HeartRateSensor));
+	if (!sensor) {
+		return NULL;
+	}
+	sensor->random = newRandom();
+	if (!sensor->random) {
+		deleteHeartRateSensor(sensor);
+		sensor = NULL;
+	}
+	return sensor;
 }
 
 void deleteHeartRateSensor(HeartRateSensor target) {
@@ -17,12 +29,14 @@ void deleteHeartRateSensor(HeartRateSensor target) {
 
 
 bool HeartRateSensor_getBatteryLevel(HeartRateSensor target, int_t *result) {
-	// ToDo: Set the value(s) of *result (if data type is a struct, copy all values) and then return true on success or false on failure.
+	*result = 90;
+	printf("getBatteryLevel() = %i\n", *result);
 	return true;
 }
 
 bool HeartRateSensor_getHeartRate(HeartRateSensor target, int_t *result) {
-	// ToDo: Set the value(s) of *result (if data type is a struct, copy all values) and then return true on success or false on failure.
+	*result = Random_next(target->random, MIN_HEART_RATE, MAX_HEART_RATE + 1);
+	printf("getHeartRate() = %i\n", *result);
 	return true;
 }
 
