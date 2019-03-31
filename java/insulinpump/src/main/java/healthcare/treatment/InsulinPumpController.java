06af4e46c4a95aa413c3755e56cfc99bf7a6d678
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2019-03-31 19:37:38 +0200

    Implement InsulinPump

--- java/insulinpump/src/main/java/healthcare/treatment/InsulinPumpController.java
+++ java/insulinpump/src/main/java/healthcare/treatment/InsulinPumpController.java
@@ -1,7 +1,13 @@
 package healthcare.treatment;
 
 // Start of user code for imports
+import java.time.Duration;
 import java.util.*;
+
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.TaskScheduler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pusztai.thomas.architecture.fog.validation.*;
 import healthcare.treatment.constraints.*;
 
@@ -11,6 +17,8 @@ import healthcare.models.InsulinPumpState;
 
 import healthcare.treatment.InsulinPump;
 import io.micronaut.http.annotation.Controller;
+
+import javax.inject.Named;
 // End of user code
 
 /**
@@ -21,37 +29,64 @@ import io.micronaut.http.annotation.Controller;
 @Controller("/insulinpump")
 public class InsulinPumpController implements InsulinPump {
 
+	private static final Logger LOG = LoggerFactory.getLogger(InsulinPumpController.class);
+
+	private InsulinPumpState currState;
+	private final TaskScheduler taskScheduler;
+
+	public InsulinPumpController(@Named(TaskExecutors.SCHEDULED) TaskScheduler taskScheduler) {
+		this.taskScheduler = taskScheduler;
+		currState = new InsulinPumpState();
+		currState.setTimeLastBolusEnded(0);
+		currState.setInsulinUnitsAdministeredLast24Hours(0);
+		currState.setInsulinUnitsAdministeredLastHour(0);
+		TreatmentPlan plan = new TreatmentPlan();
+		BasalRate defaultRate = new BasalRate();
+		defaultRate.setInsulinUnitsPerHour(2);
+		plan.setBasalRate(defaultRate);
+		plan.setMaxHourlyBasalRate(7);
+		plan.setMaxInsulinUnitsPerDay(100);
+		plan.setMaxInsulinUnitsPerHour(15);
+		plan.setMaxUnitsPerBolus(10);
+		plan.setMinHourlyBasalRate(1);
+		currState.setTreatmentPlan(plan);
+	}
 
 	@Override
 	public void setCurrentTime(Integer timestamp) {
-		// ToDo: Implement this method.
-		throw new UnsupportedOperationException("This method is not yet implemented");
+		LOG.info("setCurrentTime({})", timestamp);
+		if (currState != null) {
+			currState.setCurrentTime(timestamp);
+		}
 	}
 	
 	@Override
 	@PreCondition(PreStartBolus.class)
 	public void startBolus(Bolus bolus) {
-		// ToDo: Implement this method.
-		throw new UnsupportedOperationException("This method is not yet implemented");
+		LOG.info("startBolus({} units, {} minutes)", bolus.getInsulinUnits(), bolus.getDurationMinutes());
+		currState.setBolus(bolus);
+		currState.setInsulinUnitsAdministeredLast24Hours(currState.getInsulinUnitsAdministeredLast24Hours() + bolus.getInsulinUnits());
+		currState.setInsulinUnitsAdministeredLastHour(currState.getInsulinUnitsAdministeredLastHour() + bolus.getInsulinUnits());
+		taskScheduler.schedule(Duration.ofMinutes(bolus.getDurationMinutes()), () -> currState.setBolus(null));
 	}
 	
 	@Override
 	public InsulinPumpState getCurrentState() {
-		// ToDo: Implement this method.
-		throw new UnsupportedOperationException("This method is not yet implemented");
+		LOG.info("getCurrentState()");
+		return this.currState;
 	}
 	
 	@Override
 	@PreCondition(PreAdjustBasalRate.class)
 	public void adjustBasalRate(BasalRate rate) {
-		// ToDo: Implement this method.
-		throw new UnsupportedOperationException("This method is not yet implemented");
+		LOG.info("adjustBasalRate({} units per hour)", rate.getInsulinUnitsPerHour());
+		currState.getTreatmentPlan().setBasalRate(rate);
 	}
 	
 	@Override
 	public Integer getBatteryLevel() {
-		// ToDo: Implement this method.
-		throw new UnsupportedOperationException("This method is not yet implemented");
+		LOG.info("getBatteryLevel() = 80");
+		return 80;
 	}
 	
 
